3f02095a8b7a064f51ebe275bb92fc63704639ef
githubfengbozhang <a452799018@sina.com>
2019-11-24 23:01:10 +0800

    裁剪图片问题

--- src/comment/global_variable.js
+++ src/comment/global_variable.js
@@ -4,8 +4,8 @@
  * @LastEditors: fengbozhang
  * @LastEditTime: 2019-10-31 15:19:46
  */
-const ANDROID_APP = 'teacher'
-// const ANDROID_APP = 'student'
+// const ANDROID_APP = 'teacher'
+const ANDROID_APP = 'student'
 export default {
   ANDROID_APP
 }
